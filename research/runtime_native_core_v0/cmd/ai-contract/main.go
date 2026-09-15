cd89cc9de1642e785e259edfc4f4d9e90fa4a3f0
Unjuno <unjuno.org@unjuno.org>
2026-09-15 23:28:34 +0900

    Align native conformance CLI with frozen local source

--- research/runtime_native_core_v0/cmd/ai-contract/main.go
+++ research/runtime_native_core_v0/cmd/ai-contract/main.go
@@ -13,14 +13,20 @@ func main() {
 		os.Exit(2)
 	}
 	b, e := os.ReadFile(os.Args[1])
-	if e != nil { panic(e) }
+	if e != nil {
+		panic(e)
+	}
 	var p nc.Program
-	if e = json.Unmarshal(b, &p); e != nil { panic(e) }
+	if e = json.Unmarshal(b, &p); e != nil {
+		panic(e)
+	}
 	if e = nc.ValidateProgram(p); e != nil {
 		fmt.Fprintln(os.Stderr, e)
 		os.Exit(1)
 	}
 	s, e := nc.EncodeC1(p)
-	if e != nil { panic(e) }
+	if e != nil {
+		panic(e)
+	}
 	fmt.Println(s)
 }
