ac8a4a3422bbac608f2eb9105154e664a0529fe6
Unjuno <unjuno.org@unjuno.org>
2026-09-18 19:28:31 +0900

    Strengthen #1449 terminal authority audit

--- research/measurement/concurrent_fast_decision_t2_async_return_preflight_v1/audit.py
+++ research/measurement/concurrent_fast_decision_t2_async_return_preflight_v1/audit.py
@@ -19,13 +19,23 @@ def evaluate(r):
         if any(c.get('threads_alive',{}).values()): integ.append(cid+':thread_alive')
         if not isinstance(rr,int) or not isinstance(close,int): integ.append(cid+':missing_return_clock'); continue
         if close<rr: integ.append(cid+':close_before_receive')
+        if au.get('closed') is not True: integ.append(cid+':authority_not_closed')
+        if au.get('generation')!=2: integ.append(cid+':terminal_generation')
+        msg=au.get('frontier_message',{})
+        if msg.get('kind')!='RETURN': integ.append(cid+':frontier_message_kind')
+        if msg.get('child_pid')!=c.get('frontier_child_pid'): integ.append(cid+':frontier_child_pid')
+        if not isinstance(msg.get('child_send_ns'),int) or msg.get('child_send_ns')>rr: integ.append(cid+':frontier_clock_order')
+        if msg.get('parent_receive_ns')!=rr: integ.append(cid+':frontier_receive_binding')
         for s in c.get('samples',[]):
             if s.get('disposition') not in VOCAB or MAP.get(s.get('state'))!=s.get('disposition'): integ.append(cid+':selector')
         for a in c.get('admissions',[]):
             if a.get('commit_ns',0)>=rr: autherr.append(cid+':post_return_admission')
         probes=[x for x in c.get('rejections',[]) if x.get('kind')=='stale_probe']
         if len(probes)!=1: integ.append(cid+':stale_probe_shape')
-        elif probes[0].get('commit_ns',0)<rr or probes[0].get('admitted') is not False: autherr.append(cid+':stale_probe_not_rejected')
+        else:
+            pr=probes[0]
+            if pr.get('prepared_ns',10**30)>=rr: integ.append(cid+':stale_probe_not_prepared_before_return')
+            if pr.get('commit_ns',0)<rr or pr.get('admitted') is not False: autherr.append(cid+':stale_probe_not_rejected')
         hard_samples=[s for s in c.get('samples',[]) if s.get('state')==HARD]
         if hard_samples:
             ys=[s for s in hard_samples if s.get('disposition')=='YIELD']
@@ -51,6 +61,9 @@ def controls(r):
     q=copy.deepcopy(r); q['cases'][0]['child_exitcode']=9; tests['child_exit']=evaluate(q)['decision']=='FAIL_INTEGRITY'
     q=copy.deepcopy(r); q['cases'][0]['samples'][0]['disposition']='BROKEN'; tests['selector']=evaluate(q)['decision']=='FAIL_INTEGRITY'
     q=copy.deepcopy(r); q['cases'][0]['rejections']=[x for x in q['cases'][0]['rejections'] if x.get('kind')!='stale_probe']; tests['probe_missing']=evaluate(q)['decision']=='FAIL_INTEGRITY'
+    q=copy.deepcopy(r); q['cases'][0]['authority']['generation']=1; tests['terminal_generation']=evaluate(q)['decision']=='FAIL_INTEGRITY'
+    q=copy.deepcopy(r); q['cases'][0]['authority']['frontier_message']['child_pid']=-1; tests['child_pid_binding']=evaluate(q)['decision']=='FAIL_INTEGRITY'
+    q=copy.deepcopy(r); q['cases'][0]['rejections'][0]['prepared_ns']=q['cases'][0]['authority']['return_receive_ns']+1; tests['probe_prepared_after_return']=evaluate(q)['decision']=='FAIL_INTEGRITY'
     return tests
 
 def main():
