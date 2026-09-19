135a3c996316f0fc3de6d2e4e3c1722357b35d1d
Kilo <kilo@local>
2026-09-20 01:58:11 +0900

    test: make host IPC path assertion portable

--- runtime/test_host_model_ipc_broker_v1.py
+++ runtime/test_host_model_ipc_broker_v1.py
@@ -5,7 +5,8 @@ import unittest
 class HostBrokerContractTest(unittest.TestCase):
     def test_maps_container_repo_paths(self):
         from runtime.host_model_ipc_broker_v1 import host_path
-        self.assertEqual(host_path("/repo/runtime/a.png", Path("C:/repo")), "C:/repo/runtime/a.png")
+        self.assertEqual(Path(host_path("/repo/runtime/a.png", Path("C:/repo"))),
+                         Path("C:/repo/runtime/a.png"))
         self.assertIsNone(host_path(None, Path("C:/repo")))
 
     def test_broker_is_non_authoritative(self):
